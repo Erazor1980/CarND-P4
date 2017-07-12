dfa8d25ebd1d993f0f83b8aec3bda2e9a32b4b07
Lukas <lukas.caup@gmx.de>
2017-07-12 02:40:26 +0200

    - camera calibraton implemented

--- camera_calibration.py
+++ camera_calibration.py
@@ -4,15 +4,30 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import glob
 
+# pre-calculated values
+'''
+Camera matrix:
+ [[  1.15396093e+03   0.00000000e+00   6.69705359e+02]
+ [  0.00000000e+00   1.14802495e+03   3.85656232e+02]
+ [  0.00000000e+00   0.00000000e+00   1.00000000e+00]]
+Distortion coefficients:
+ [[ -2.41017968e-01  -5.30720497e-02  -1.15810318e-03  -1.28318544e-04   2.67124303e-02]]
+'''
+
 # for showing found corners for each image
-show_corner_images = True
+show_corner_images = False
 
 img_points = [] # 2D points in image plane
-obf_points = [] # 3d points in real world
+obj_points = [] # 3d points in real world
+
+# preparation of object points (same for all images)
+objp = np.zeros((6 * 9, 3), np.float32)
+objp[:,:2] = np.mgrid[0:9, 0:6].T.reshape(-1,2)     # x, y coordinates, z = 0
 
 # store calibration image names in a list
 calib_images = glob.glob('./camera_cal/calibration*.jpg')
 
+print("Loading images and finding chessboard corners...")
 for img_name in calib_images:
     # load current image
     img = mpimg.imread(img_name)
@@ -21,12 +36,12 @@ for img_name in calib_images:
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
     # find the chessboard corners
-    ret, corners = cv2.findChessboardCorners(gray, (9,6))
+    ret, corners = cv2.findChessboardCorners(gray, (9, 6))
 
     # add object points and image points if corners found
     if ret == True:
         img_points.append(corners)
-        #TODO obf_points
+        obj_points.append(objp)
 
         if show_corner_images == True:
             img = cv2.drawChessboardCorners(img, (9, 6), corners, ret)
@@ -34,3 +49,9 @@ for img_name in calib_images:
             cv2.imshow('Corners', img)
             cv2.waitKey(250)
 
+# CAMERA CALIBRATION
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, gray.shape[::-1], None, None)
+
+print("done!\nCamera calibration successfull.")
+print("Camera matrix:\n", mtx)
+print("Distortion coefficients:\n", dist)
